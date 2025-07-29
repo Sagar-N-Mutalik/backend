75ec4bee2f1a9132447d3d28b6720a9512e20632
sagar n mutalik <sagarnmutalik6@gmailcom>
2025-07-29 17:38:03 +0530

    fixing error

--- backend/src/main/java/com/todoapp/entity/User.java
+++ backend/src/main/java/com/todoapp/entity/User.java
@@ -1,9 +1,10 @@
-// sagar-n-mutalik/backend/backend-8504ec860ca0048563c538b23a17e80f4ea23756/backend/src/main/java/com/todoapp/entity/User.java
+// sagar-n-mutalik/backend/backend-e19bddeb0450ef02ae010ebb00e23fc2250b02a4/backend/src/main/java/com/todoapp/entity/User.java
 package com.todoapp.entity;
 
 import java.util.ArrayList;
 import java.util.List;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor; // Import this
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 @Data
 @NoArgsConstructor
-@Builder // Add this annotation
+@AllArgsConstructor // Add this annotation
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,10 +29,5 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Todo> todos = new ArrayList<>();
 
-    public User(Long id, String username, String password, List<Todo> todos) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.todos = todos;
-    }
+
 }
